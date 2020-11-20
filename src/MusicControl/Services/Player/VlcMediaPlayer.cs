df6d7a964806edfe0b2c64f9556afea13b3cb7dc
Portmann Peter <pportman@gemalto.com>
2020-11-20 13:02:15 +0100

    VlcDummy added to simulate VLC media player.

---
+++ src/MusicControl/Services/Player/VlcMediaPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Sockets;
+
+namespace PeKaRaSa.MusicControl.Services.Player
+{
+    /// <summary>
+    /// Communicates via mpc with the music player deamon (mpd)
+    /// </summary>
+    /// <remarks>
+    /// Alternatively it would be possible to communicate directly with the mpd.
+    /// <seealso cref="https://www.musicpd.org/doc/html/protocol.html"/>
+    /// </remarks>
+    public class VlcMediaPlayer : IMusicPlayerClient
+    {
+        private Int32 _port;
+
+        public VlcMediaPlayer()
+        {
+            if (!Int32.TryParse(ConfigurationManager.AppSettings["vlcPort"], out _port))
+            {
+                // Fallback: Set the port 13001.
+                _port = 13001;
+            }
+        }
+
+        public void Send(string command)
+        {
+            using (var tcpClient = new TcpClient("localhost", _port))
+            using (var stream = tcpClient.GetStream())
+            {
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(command);
+                stream.Write(data, 0, data.Length);
+                // echo "$1" | netcat localhost 12345 -q 1
+                stream.Close();
+                tcpClient.Close();
+            }
+        }
+    }
+}
---
+++ src/VlcDummy/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace VlcDummy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(localAddr, 13001);
+            Console.WriteLine($"vlc is listening on {localAddr} : 13001");
+
+            // Start listening for client requests.
+            server.Start();
+
+            while (true)
+            {
+                Console.Write("-");
+                using (TcpClient client = server.AcceptTcpClient())
+                {
+                    Console.Write(">");
+
+                    byte[] dataBuffer = new byte[256];
+                    Int32 byteCount;
+
+                    // Get a stream object for reading and writing
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        // Read the first batch of the TcpServer response bytes.
+                        byteCount = stream.Read(dataBuffer, 0, dataBuffer.Length);
+                        stream.Close();
+                    }
+
+                    // String to store the response ASCII representation.
+                    string responseData = System.Text.Encoding.ASCII.GetString(dataBuffer, 0, byteCount);
+                    Console.WriteLine("{0}", responseData);
+
+                    if (responseData.StartsWith("shutdown"))
+                    {
+                        client.Close();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
