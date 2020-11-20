574b12f69d97764bce2870eae3bdf101fda0052c
Portmann Peter <pportman@gemalto.com>
2020-11-20 21:05:13 +0100

    setcd: Reads and removes the first line of setcd.dummy. Appends this line at the end and returns it.

--- src/DummyConsoleApplication/Program.cs
+++ src/DummyConsoleApplication/Program.cs
@@ -6,9 +6,21 @@ namespace PeKaRaSa.SetcdDummy
 {
     class Program
     {
+        /// <summary>
+        /// Reads and removes the first line of setcd.dummy, appends it at the end and returns it
+        /// </summary>
+        /// <param name="args"></param>
+
         static void Main(string[] args)
         {
-            Console.WriteLine(File.ReadLines("setcd.dummy").First());
+            var allLines = File.ReadAllLines("setcd.dummy").ToList();
+            string firstLine = allLines.First();
+            allLines.Add(firstLine);
+            allLines.RemoveAt(0);
+
+            File.WriteAllLines("setcd.dummy", allLines);
+
+            Console.WriteLine(firstLine);
         }
     }
 }
