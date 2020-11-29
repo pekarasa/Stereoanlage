7501c4b8930c0d00b0ac9f08219f027a2d742452
Peter Portmann <pekarasa@gmx.ch>
2020-11-29 20:07:15 +0100

    folder src should not be deployed on raspberry pi

--- deployAll.sh
+++ deployAll.sh
@@ -1,7 +1,8 @@
 sudo cp ~/VLR-RC001_0026.lircd.conf /etc/lirc/lircd.conf.d/
-mkdir /home/pi/mpd /home/pi/mpd/music/ /home/pi/mpd/playlists/ /home/pi/mpd/music/mnt/
+mkdir /home/pi/mpd/music/mnt/
 sudo chmod g+w /home/pi/mpd/music/ /home/pi/mpd/playlists/ /home/pi/mpd/music/mnt/
 sudo chgrp audio /home/pi/mpd/music/ /home/pi/mpd/playlists/ /home/pi/mpd/music/mnt/
 sudo cp /home/pi/irexec.init.d /etc/init.d/irexec
 sudo chmod 755 /etc/init.d/irexec
 sudo chmod 755 /home/pi/control/musicCenter.sh
+sudo cp -R /home/pi/srv/MusicControl /srv
