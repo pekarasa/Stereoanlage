b5d867649dfb2aebef5c1c1d7ac8e115d275ddd2
Peter Portmann <pekarasa@gmx.ch>
2021-01-21 16:52:58 +0100

    Comment added and small reafctorings

--- src/MusicControl/IAudioUnitFactory.cs
+++ src/MusicControl/IAudioUnitFactory.cs
@@ -8,6 +8,13 @@ namespace PeKaRaSa.MusicControl
     /// </summary>
     public interface IAudioUnitFactory
     {
+        /// <summary>
+        /// Returns an instance of the <paramref name="unitToActivate"/> or null if the requested unit does not exist.
+        /// </summary>
+        /// <param name="unitToActivate">requested unit to activate</param>
+        /// <param name="currentUnit">unit to stop</param>
+        /// <param name="token"></param>
+        /// <returns></returns>
         public IAudioUnit GetActiveUnit(string unitToActivate, IAudioUnit currentUnit, CancellationToken token);
 
         public IAudioUnit GetDefaultUnit();
