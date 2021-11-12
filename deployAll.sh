41a9ad1771382699a278deb362b2f0e3e8322ed5
Peter Portmann <pekarasa@gmx.ch>
2021-11-12 17:23:45 +0100

    fix: restartVlc.sh removed

--- deployAll.sh
+++ deployAll.sh
@@ -4,7 +4,6 @@ sudo chmod g+w /home/pi/mpd/music/ /home/pi/mpd/playlists/ /home/pi/mpd/music/mn
 sudo chgrp audio /home/pi/mpd/music/ /home/pi/mpd/playlists/ /home/pi/mpd/music/mnt/
 sudo cp /home/pi/irexec.init.d /etc/init.d/irexec
 sudo chmod 755 /etc/init.d/irexec
-sudo chmod 755 /home/pi/control/restartVlc.sh
 sudo chmod 755 /home/pi/control/powerOff.sh
 sudo cp -R /home/pi/srv/MusicControl /srv
 sudo cp MusicControl.service /etc/systemd/system/MusicControl.service
